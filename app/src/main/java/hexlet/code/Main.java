9f6f5ab3c9ec5cfaca74ca67d1f7e978b4cacafa
hplskid <hplskid@gmail.com>
2023-04-18 21:32:47 +0300

    Initialize picocli project, add makefile

--- app/src/main/java/hexlet/code/Main.java
+++ app/src/main/java/hexlet/code/Main.java
@@ -1,9 +1,25 @@
 package hexlet.code;
 
-public class Main {
+import java.util.concurrent.Callable;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(
+    name = "gendiff",
+    mixinStandardHelpOptions = true,
+    version = "1.0.0-DEV",
+    description = "Compares two configuration files and shows a difference."
+)
+public class Main implements Callable<Integer> {
+
+    @Override
+    public Integer call() {
+        return 0;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
     }
 
 }
---
+++ app/src/main/java/hexlet/code/differ/Differ.java
@@ -0,0 +1,13 @@
+package hexlet.code.differ;
+
+/**
+ * Created by Ilya Tankov on 4/18/2023
+ **/
+public class Differ {
+
+    //TODO: Make that method return difference between two files
+    public static String generate(String filePathOne, String filePathTwo) {
+
+        return "";
+    }
+}
